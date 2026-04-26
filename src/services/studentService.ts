635d981475e75fcdd1576ca213df6ff910a7fcbb
Patty <patriciamarie0816@gmail.com>
2026-04-26 22:00:53 +0800

    updated student service after testing endpoints

--- src/services/studentService.ts
+++ src/services/studentService.ts
@@ -55,7 +55,7 @@ export const updateStudent = async (
         .update(updates)
         .eq("id", id)
         .select()
-        .single();
+        .maybeSingle();
 
     if (error) throw error;
     return data;
